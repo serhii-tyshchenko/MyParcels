4a691df3bd7d3a73404e890cf4eb601a4a9a4d99
Serhii Tyshchenko (EPAM) <serhii_tyshchenko1@epam.com>
2021-12-21 23:31:22 +0200

    add stories

---
+++ src/modules/ui/UIIconButton/UIIconButton.stories.js
@@ -0,0 +1,8 @@
+import { UIIconButton } from './UIIconButton';
+
+export default {
+  title: 'UI / UIIconButton',
+  component: UIIconButton,
+};
+
+export const Default = (args) => <UIIconButton {...args} />;
